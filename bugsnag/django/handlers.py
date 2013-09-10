2b73c6f5dba47f1ea7dadf1ea515408b1af287e0
arski <martin@webscio.net>
2013-09-10 17:35:38 +0200

    Whitespace fixes

--- bugsnag/django/handlers.py
+++ bugsnag/django/handlers.py
@@ -7,13 +7,14 @@ import bugsnag
 class BugsnagHandler(logging.Handler):
     def __init__(self):
         logging.Handler.__init__(self)
+
         # Ignore django 404s by default
         bugsnag.configuration.ignore_classes.append("django.http.Http404")
 
         # Import Bugsnag settings from settings.py
         django_bugsnag_settings = getattr(settings, 'BUGSNAG', {})
         bugsnag.configure(**django_bugsnag_settings)
-    
+
     def emit(self, record):
         print record.__dict__.keys()
         if record.exc_info:
