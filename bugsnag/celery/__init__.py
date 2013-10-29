a148d2b277b9f01e86e526287b2047f4868f76a8
Conrad Irwin <conrad.irwin@gmail.com>
2013-10-28 21:40:09 -0700

    Send traceback and arguments

--- bugsnag/celery/__init__.py
+++ bugsnag/celery/__init__.py
@@ -3,14 +3,14 @@ from celery.signals import task_failure
 import bugsnag
 
 def process_failure_signal(sender, task_id, exception, args, kwargs, traceback, einfo, **kw):
-    if hasattr(einfo, 'exc_info'):
-        # for Celery 2.4 or later
-        exc_info = einfo.exc_info
-    else:
-        # for Celery before 2.4
-        exc_info = (type(exception), exception, traceback)
+    task = {
+        "task_id": task_id,
+        "args": args,
+        "kwargs": kwargs
+    }
 
-    # Send the exception to bugsnag
-    bugsnag.notify(exception, context="celery", extra_data={"task_id": task_id})
+    bugsnag.notify(exception, traceback=traceback,
+                              context=sender.name,
+                              extra_data=task)
 
 task_failure.connect(process_failure_signal, weak=False)
