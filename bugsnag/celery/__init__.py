94421f090e983cf8fd8bda2fbf6cfd9692ee0a78
Conrad Irwin <conrad.irwin@gmail.com>
2013-10-29 10:51:11 -0700

    +Polish celery support a little

--- bugsnag/celery/__init__.py
+++ bugsnag/celery/__init__.py
@@ -1,8 +1,7 @@
 from celery.signals import task_failure
-
 import bugsnag
 
-def process_failure_signal(sender, task_id, exception, args, kwargs, traceback, einfo, **kw):
+def failure_handler(sender, task_id, exception, args, kwargs, traceback, einfo, **kw):
     task = {
         "task_id": task_id,
         "args": args,
@@ -13,4 +12,11 @@ def process_failure_signal(sender, task_id, exception, args, kwargs, traceback, 
                               context=sender.name,
                               extra_data=task)
 
-task_failure.connect(process_failure_signal, weak=False)
+def connect_failure_handler():
+    """
+    Connect the bugsnag failure_handler to the Celery
+    task_failure signal
+    """
+    task_failure.connect(failure_handler, weak=False)
+
+
