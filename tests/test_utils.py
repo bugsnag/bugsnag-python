99ebcef391bea7e87f4df7ee7e605d57e87f31cf
James Smith <james@loopj.com>
2014-05-13 15:38:02 -0700

    Revert "Skip flask tests on unsupported python versions"
    This reverts commit de37f475171e3097a63bd2bb664085e75259ce44.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,35 +1,31 @@
-import sys
-import unittest
 from bugsnag.utils import json_encode, sanitize_object
 from nose.tools import eq_
 
-@unittest.skipIf((3,0) <= sys.version_info < (3,3), "unicode string helper only support python3 >= 3.3")
-class UtilTestCase(unittest.TestCase):
-  def test_sanitize_object(self):
-      filters = ["password", "credit_card"]
-      crazy_dict = {
-          "password": "123456",
-          "metadata": {
-              "another_password": "123456",
-              "regular": "text"
-          },
-          "bad_utf8": "a test of \xe9 char",
-          "list": ["list", "of", "things"],
-          "unicode": u"string",
-          "obj": Exception(),
-          "valid_unicode": u"\u2603",
-      }
+def test_sanitize_object():
+    filters = ["password", "credit_card"]
+    crazy_dict = {
+        "password": "123456",
+        "metadata": {
+            "another_password": "123456",
+            "regular": "text"
+        },
+        "bad_utf8": "a test of \xe9 char",
+        "list": ["list", "of", "things"],
+        "unicode": u"string",
+        "obj": Exception(),
+        "valid_unicode": u"\u2603",
+    }
 
-      # Sanitize our object
-      sane_dict = sanitize_object(crazy_dict, filters=filters)
+    # Sanitize our object
+    sane_dict = sanitize_object(crazy_dict, filters=filters)
 
-      # Check the values have been sanitized
-      assert(sane_dict["password"] == "[FILTERED]")
-      assert(sane_dict["metadata"]["another_password"] == "[FILTERED]")
-      assert(sane_dict["metadata"]["regular"] == "text")
-      assert("things" in sane_dict["list"])
+    # Check the values have been sanitized
+    assert(sane_dict["password"] == "[FILTERED]")
+    assert(sane_dict["metadata"]["another_password"] == "[FILTERED]")
+    assert(sane_dict["metadata"]["regular"] == "text")
+    assert("things" in sane_dict["list"])
 
-  def test_json_encode(self):
+def test_json_encode():
 
-      payload = {"a": u"a" * 512 * 1024}
-      eq_(json_encode(payload), ('{"a": "' + 'a' * 1024 + '"}').encode('utf-8', 'replace'))
+    payload = {"a": u"a" * 512 * 1024}
+    eq_(json_encode(payload), ('{"a": "' + 'a' * 1024 + '"}').encode('utf-8', 'replace'))
