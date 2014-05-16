2f1ea86d43d378d263322911690e58709323471e
Cal Leeming <cal@iops.io>
2014-05-16 23:51:55 +0100

    Added better log handler support with severity

--- bugsnag/handlers.py
+++ bugsnag/handlers.py
@@ -4,17 +4,28 @@ import logging
 import bugsnag
 
 class BugsnagHandler(logging.Handler):
-    def __init__(self):
-        logging.Handler.__init__(self)
+    def __init__(self, api_key=None):
+        super(BugsnagHandler, self).__init__()
 
-        # To enable support for django we try to run the django configure here, but 
-        # fail gracefully if we couldnt import or it was not configured properly
-        try:
-          import bugsnag.django
-          bugsnag.django.configure()
-        except:
-          pass
+        # Check if API key has been specified.
+        if not bugsnag.configuration.api_key and not api_key:
+            raise Exception, "API key has not been configured or specified"
+
+        # If API key specified in logger, always use this value, even if another
+        # value was configured previously
+        if api_key:
+            bugsnag.configuration.api_key = api_key
 
     def emit(self, record):
+        if record.levelname.lower() in ['error', 'critical']:
+            severity = 'error'
+        elif record.levelname.lower() in ['warning', ]:
+            severity = 'warning'
+        else:
+            severity = 'info'
+
         if record.exc_info:
-            bugsnag.notify(record.exc_info)
+            bugsnag.notify(record.exc_info, severity=severity)
+        else:
+            exc = Exception(record.message)
+            bugsnag.notify(exc, severity=severity)
