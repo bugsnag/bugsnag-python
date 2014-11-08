250b5dffb3e9f006e2dc1c92049bb77be45ff8f7
Stas SUȘCOV <stas@net.utcluj.ro>
2014-11-08 18:59:26 +0200

    Pep cleanups. Lots of unused modules removed.

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -2,25 +2,24 @@ import sys
 from nose.plugins.skip import SkipTest
 if (3,0) <= sys.version_info < (3,3): raise SkipTest("Flask is incompatible with python3 3.0 - 3.2")
 
-import os
-import socket
-from webtest import TestApp
-from nose.tools import eq_, assert_raises
-from mock import MagicMock, patch
+from nose.tools import eq_
+from mock import patch
 
 from flask import Flask
-from bugsnag.six import Iterator
 from bugsnag.flask import handle_exceptions
 import bugsnag.notification
 
 bugsnag.configuration.api_key = '066f5ad3590596f9aa8d601ea89af845'
 
+
 class SentinalError(RuntimeError):
     pass
+
 
 @patch('bugsnag.notification.deliver')
 def test_bugsnag_middleware_working(deliver):
     app = Flask("working")
+
     @app.route("/hello")
     def hello():
         return "OK"
@@ -32,49 +31,55 @@ def test_bugsnag_middleware_working(deliver):
 
     eq_(deliver.call_count, 0)
 
+
 @patch('bugsnag.notification.deliver')
 def test_bugsnag_crash(deliver):
     app = Flask("crashing")
+
     @app.route("/hello")
     def hello():
         raise SentinalError("oops")
 
     handle_exceptions(app)
-    resp = app.test_client().get('/hello')
+    app.test_client().get('/hello')
 
     eq_(deliver.call_count, 1)
     payload = deliver.call_args[0][0]
     eq_(payload['events'][0]['exceptions'][0]['errorClass'], 'test_flask.SentinalError')
     eq_(payload['events'][0]['metaData']['request']['url'], 'http://localhost/hello')
 
+
 @patch('bugsnag.notification.deliver')
 def test_bugsnag_notify(deliver):
     app = Flask("notifying")
+
     @app.route("/hello")
     def hello():
         bugsnag.notify(SentinalError("oops"))
         return "OK"
 
     handle_exceptions(app)
-    resp = app.test_client().get('/hello')
+    app.test_client().get('/hello')
 
     eq_(deliver.call_count, 1)
     payload = deliver.call_args[0][0]
     eq_(payload['events'][0]['metaData']['request']['url'], 'http://localhost/hello')
 
+
 @patch('bugsnag.notification.deliver')
 def test_bugsnag_custom_data(deliver):
-    meta_data = [{"hello":{"world":"once"}}, {"again":{"hello":"world"}}]
+    meta_data = [{"hello": {"world": "once"}}, {"again": {"hello": "world"}}]
 
     app = Flask("custom")
+
     @app.route("/hello")
     def hello():
         bugsnag.configure_request(meta_data=meta_data.pop())
         raise SentinalError("oops")
 
     handle_exceptions(app)
-    resp = app.test_client().get('/hello')
-    resp = app.test_client().get('/hello')
+    app.test_client().get('/hello')
+    app.test_client().get('/hello')
 
     eq_(deliver.call_count, 2)
 
