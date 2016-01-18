57c0905192444c53644789474ce221652397ed21
Delisa Mason <iskanamagus@gmail.com>
2016-01-18 15:21:26 +0000

    Release 2.4.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ tests_require = [
 
 setup(
     name='bugsnag',
-    version='2.3.1',
+    version='2.4.0',
     description='Automatic error monitoring for django, flask, etc.',
     long_description=__doc__,
     author='Simon Maynard',
