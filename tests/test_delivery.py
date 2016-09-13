a017b17fda925ae3b275cf518de5166ec47e34ce
Delisa Mason <iskanamagus@gmail.com>
2016-09-13 16:48:54 +0100

    Test use_ssl fallback in delivery

--- tests/test_delivery.py
+++ tests/test_delivery.py
@@ -16,8 +16,16 @@ class DeliveryTest(IntegrationTest):
         self.config = Configuration()
         self.config.configure(api_key='abc',
                               endpoint=self.server.address,
-                              asynchronous=False,
-                              use_ssl=False)
+                              use_ssl=False,
+                              asynchronous=False)
+
+    def test_urllib_delivery_full_url(self):
+        self.config.configure(endpoint=self.server.url, use_ssl=None)
+        UrllibDelivery().deliver(self.config, '{"legit":4}')
+
+        self.assertSentReportCount(1)
+        request = self.server.received[0]
+        self.assertEqual(request['json_body'], {"legit": 4})
 
     def test_urllib_delivery(self):
         UrllibDelivery().deliver(self.config, '{"legit":4}')
@@ -44,6 +52,23 @@ class DeliveryTest(IntegrationTest):
         self.assertEqual(request['json_body'], {"legit": 4})
         self.assertEqual(request['headers']['Content-Type'],
                          'application/json')
+
+    def test_requests_delivery_full_url(self):
+        if sys.version_info < (2, 7):
+            raise SkipTest("Requests is incompatible with 2.6")
+
+        try:
+            import requests  # noqa
+        except:
+            raise SkipTest("Requests is not installed")
+
+        self.config.configure(endpoint=self.server.url)
+        del self.config.use_ssl
+        RequestsDelivery().deliver(self.config, '{"legit":4}')
+
+        self.assertSentReportCount(1)
+        request = self.server.received[0]
+        self.assertEqual(request['json_body'], {"legit": 4})
 
     def test_create_default_delivery(self):
         delivery = create_default_delivery()
