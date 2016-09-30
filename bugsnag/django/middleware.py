38ad88add12e942c59e82c297a26fbae950bc80a
Jonathan Pickett <jonathanpickett@me.com>
2016-09-29 22:51:50 -0700

    Have BugsnagMiddleware for Django inherit from Django MiddlewareMixin for 1.10 compatibility

--- bugsnag/django/middleware.py
+++ bugsnag/django/middleware.py
@@ -1,12 +1,15 @@
 from __future__ import division, print_function, absolute_import
 
+from django.utils.deprecation import MiddlewareMixin
+
 import bugsnag
 import bugsnag.django
 
 
-class BugsnagMiddleware(object):
-    def __init__(self):
+class BugsnagMiddleware(MiddlewareMixin):
+    def __init__(self, get_response=None):
         bugsnag.django.configure()
+        super(BugsnagMiddleware, self).__init__(get_response)
 
     # pylint: disable-msg=R0201
     def process_request(self, request):
