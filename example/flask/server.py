9088d0471f163ee36fbfb2cd904171d4197a2cb5
Alex Moinet <alex@bugsnag.com>
2017-08-15 18:16:10 +0100

    Updated strings and removed unnecessary imports

--- example/flask/server.py
+++ example/flask/server.py
@@ -3,13 +3,12 @@ import bugsnag
 
 # Import platform specific Bugsnag features
 from bugsnag.flask import handle_exceptions
-from bugsnag.handlers import BugsnagHandler
 
 app = Flask(__name__)
 
 # Configure Bugsnag
 bugsnag.configure(
-    api_key = "YOUR_API_KEY_HERE"
+    api_key = 'YOUR_API_KEY_HERE'
 )
 
 # Attach Bugsnag to flasks exception handler
@@ -24,63 +23,63 @@ def crashzero():
 @app.route('/crashdict')
 def crashdict():
     customheader = request.headers['my-custom-header']
-    return "Received your header: " + customheader
+    return 'Received your header: ' + customheader
 
 # Will crash but attach diagnostic data through a callback function before reporting the error
 @app.route('/crashcallback')
 def crashcallback():
     bugsnag.before_notify(callback)
-    raise(Exception("Flask demo: Everything is fine, check the Diagnostics tab at <a href=\"bugsnag.com\">bugsnag.com</a>"))
+    raise(Exception('Flask demo: Everything is fine, check the Diagnostics tab at <a href=\"bugsnag.com\">bugsnag.com</a>'))
 
 def callback(notification):
-    if notification.context == "GET /crashcallback":
-        notification.add_tab("Diagnostics", {
-            "message": "Flask demo: Everything is fine",
-            "status": 200
+    if notification.context == 'GET /crashcallback':
+        notification.add_tab('Diagnostics', {
+            'message': 'Flask demo: Everything is fine',
+            'status': 200
         })
 
 # Will notify Bugsnag of an exception manually, which can be used to notify Bugsnag of handled errors
 @app.route('/notify')
 def notify():
-    bugsnag.notify(Exception("Flask demo: Manual notification"))
-    return "The app hasn't crashed, but check <a href=\"bugsnag.com\">bugsnag.com</a> to view notifications"
+    bugsnag.notify(Exception('Flask demo: Manual notification'))
+    return 'The app hasn\'t crashed, but check <a href=\"bugsnag.com\">bugsnag.com</a> to view notifications'
 
 # Will notify Bugsnag of an exception, with some metadata attached for better debugging
 @app.route('/notifywithmetadata')
 def notifywithmetadata():
     bugsnag.notify(
-        Exception("Flask demo: Manual notification with metadata"),
+        Exception('Flask demo: Manual notification with metadata'),
         meta_data = {
-            "Request info": {
-                "route": "notifywithmetadata", 
-                "headers": request.headers
+            'Request info': {
+                'route': 'notifywithmetadata', 
+                'headers': request.headers
             }, 
-            "Resolve info": {
-                "status": 200, 
-                "message": "Metadata has been added to this notificaiton"
+            'Resolve info': {
+                'status': 200, 
+                'message': 'Metadata has been added to this notificaiton'
             }
         },
     )
-    return ("Metadata was added to the notification, check <a href=\"bugsnag.com\">bugsnag.com</a> " +
-        " to view on the 'Request info' and 'Resolve info' tabs")
+    return ('Metadata was added to the notification, check <a href=\"bugsnag.com\">bugsnag.com</a> ' +
+        ' to view on the "Request info" and "Resolve info" tabs')
 
 # Will notify Bugsnag of an exception, and include a context included for grouping
 @app.route('/notifywithcontext')
 def notifywithcontext():
     bugsnag.notify(
-        Exception("Flask demo: Manual notification with context"),
-        context = "notifywithcontext",
+        Exception('Flask demo: Manual notification with context'),
+        context = 'notifywithcontext'
     )
-    return "The context was changed to notifywithcontext for better grouping"
+    return 'The context was changed to notifywithcontext for better grouping'
 
 # Will notify Bugsnag of an exception, with a defined severity for different handling
 @app.route('/notifywithseverity')
 def notifywithseverity():
     bugsnag.notify(
-        Exception("Flask demo: Manual notificaiton with severity"),
-        severity = "info",
+        Exception('Flask demo: Manual notificaiton with severity'),
+        severity = 'info',
     )
-    return "The severity was set to info, check <a href=\"bugsnag.com\">bugsnag.com</a> to see the difference"
+    return 'The severity was set to info, check <a href=\"bugsnag.com\">bugsnag.com</a> to see the difference'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(port=3000)
