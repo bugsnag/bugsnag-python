f108194544a0928cd0879f8b8aea3ca74546debc
Delisa Mason <iskanamagus@gmail.com>
2017-09-27 15:27:58 +0100

    Add example WSGI app

---
+++ example/wsgi/app.py
@@ -0,0 +1,15 @@
+import bugsnag
+
+from bugsnag.wsgi.middleware import BugsnagMiddleware
+
+bugsnag.configure(
+    api_key="some-api-key"
+)
+
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    raise Exception('Ack')
+    return [b"Hello World"]
+
+application = BugsnagMiddleware(application)
