6ba784fb3a056617c8f3609ea359446361a449eb
Alex Moinet <alex@bugsnag.com>
2018-05-09 17:15:50 +0100

    tests(Django): Move Django tests from example/django to tests/fixtures/django - Fix linter issues

--- tests/fixtures/django1/demo/views.py
+++ tests/fixtures/django1/demo/views.py
@@ -9,8 +9,7 @@ def index(request):
 
 
 def crash(request):
-    raise Exception("Bugsnag Django demo says: It crashed! Go check " +
-                    "bugsnag.com for a new notification!")
+    raise Exception("crash!")
 
 
 def callback(notification):
@@ -25,43 +24,36 @@ def callback(notification):
 def crash_with_callback(request):
     bugsnag.before_notify(callback)
     raise Exception(
-        "Bugsnag Django demo says: It crashed! But, due to the attached " +
-        "callback the exception has meta information. Go check " +
-        "bugsnag.com for a new notification (see the Diagnostics tab)!"
+        "crash with callback!"
     )
 
 
 def notify(request):
-    msg = "Bugsnag Django demo says: False alarm, your application didn't crash"
+    msg = "notify!"
     bugsnag.notify(Exception(msg))
-    return HttpResponse(
-        "Bugsnag Django demo says: It didn't crash! But still go check " +
-        "<a href=\"bugsnag.com\">bugsnag.com</a> for a new notification.")
+    return HttpResponse("notify!")
 
 
 def notify_meta(request):
     bugsnag.notify(
-        Exception("Bugsnag Django demo says: False alarm, your application didn't crash"),
-        Diagnostics={ "code": 200, "message": "Django demo says: Everything is great" },
-        User={ "email": "bugsnag@bugsnag.com", "username": "bob-hoskins" })
-    return HttpResponse(
-        "Bugsnag Django demo says: It didn't crash! But still go check " +
-        " <a href=\"bugsnag.com\">bugsnag.com</a> for a new notification. " +
-        "Check out the User tab for the meta data")
+        Exception("notify meta!"),
+        Diagnostics={
+            "code": 200,
+            "message": "Django demo says: Everything is great"
+        },
+        User={"email": "bugsnag@bugsnag.com", "username": "bob-hoskins"})
+    return HttpResponse("notify meta!")
 
 
 def context(request):
     bugsnag.notify(
-        Exception("Bugsnag Django demo says: Changed the context to backgroundJob"),
+        Exception("context!"),
         context="backgroundJob")
-    return HttpResponse(
-        "Bugsnag Django demo says: The context of the error is \"backgroundJob\" now")
+    return HttpResponse("context!")
 
 
 def severity(request):
     bugsnag.notify(
-        Exception("Bugsnag Django demo says: Look at the circle on the right side. It's different"),
+        Exception("severity!"),
         severity='info')
-    return HttpResponse(
-        "Bugsnag Django demo says: On <a href=\"bugsnag.com\">bugsnag.com</a> " +
-        "look at the circle on the right side. It's different")
+    return HttpResponse("severity!")
