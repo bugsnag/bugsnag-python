fa33c8f55101e35e40dfa15a6c066c2b385d1c55
Joe Haines <hello@joehaines.co.uk>
2020-06-03 14:43:00 +0100

    Update supported python versions in setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,9 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development'
     ],
     test_suite='nose.collector',
