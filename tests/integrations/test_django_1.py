6d0309eb1c90475bf640e351804639239a969270
Steve Nester <steve.nester@bugsnag.com>
2020-07-13 16:57:17 +0100

    add bad json test

--- tests/integrations/test_django_1.py
+++ tests/integrations/test_django_1.py
@@ -75,6 +75,24 @@ class DjangoMiddlewareTests(IntegrationTest):
             'GET': {}
         })
 
+    def test_notify_bad_json_post(self):
+        response = self.client.post('/notify/', 'not json',
+                                    content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(self.server.received), 1)
+        payload = self.server.received[0]['json_body']
+        event = payload['events'][0]
+        # SN: NOTE: we expect POST to be {} because bad json should not parse,
+        # however we do expect reqest metaData to be set, of course
+        self.assertEqual(event['metaData']['request'], {
+            'method': 'POST',
+            'url': 'http://testserver/notify/',
+            'path': '/notify/',
+            'POST': {},
+            'encoding': None,
+            'GET': {}
+        })
+
     def test_unhandled_exception(self):
         with self.assertRaises(Exception):
             self.client.get('/crash/')
