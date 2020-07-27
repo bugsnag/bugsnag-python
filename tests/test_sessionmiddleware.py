bd0a1d0737ca48983f6ac31e7b792e8670e2ba90
Delisa Mason <iskanamagus@gmail.com>
2020-07-27 15:04:28 +0100

    fix(sessiontracker): Ensure session counts on events do not include future events
    Time travel is against regulations
    
    [PLAT-4402]

---
+++ tests/test_sessionmiddleware.py
@@ -0,0 +1,44 @@
+import unittest
+
+from bugsnag.sessiontracker import SessionTracker, SessionMiddleware
+from bugsnag.configuration import Configuration
+from bugsnag.notification import Notification
+
+
+class TestSessionMiddleware(unittest.TestCase):
+    def setUp(self):
+        self.config = Configuration()
+        self.config.configure(api_key='fff', auto_capture_sessions=False)
+        self.sessiontracker = SessionTracker(self.config)
+        self.sessiontracker.auto_sessions = True  # Stub session delivery queue
+
+    def tearDown(self):
+        pass
+
+    def test_increment_counts(self):
+        """
+        Every event should keep a list of prior events which occurred in the
+        session
+        """
+
+        def next_callable(event):
+            pass
+
+        middleware = SessionMiddleware(next_callable)
+        self.sessiontracker.start_session()
+
+        event = Notification(Exception('shucks'), self.config, None)
+        middleware(event)
+
+        assert event.session['events']['unhandled'] == 0
+        assert event.session['events']['handled'] == 1
+
+        event2 = Notification(Exception('oh no'), self.config, None)
+        middleware(event2)
+
+        assert event2.session['events']['unhandled'] == 0
+        assert event2.session['events']['handled'] == 2
+
+        # Session counts should not change for events already handled
+        assert event.session['events']['unhandled'] == 0
+        assert event.session['events']['handled'] == 1
