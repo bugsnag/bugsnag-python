6300070d9a8d889de4b80baf05716bc99566b6be
Delisa Mason <iskanamagus@gmail.com>
2020-08-24 14:21:53 +0100

    feat(core): Add auto_notify_exc_info to capture exc_info if configured

--- bugsnag/__init__.py
+++ bugsnag/__init__.py
@@ -7,13 +7,13 @@ from bugsnag.client import Client
 from bugsnag.legacy import (configuration, configure, configure_request,
                             add_metadata_tab, clear_request_config, notify,
                             auto_notify, before_notify, start_session,
-                            send_sessions)
+                            send_sessions, auto_notify_exc_info)
 
 __all__ = ('Client', 'Notification', 'Configuration', 'RequestConfiguration',
            'configuration', 'configure', 'configure_request',
            'add_metadata_tab', 'clear_request_config', 'notify',
            'auto_notify', 'before_notify', 'start_session',
-           'send_sessions')
+           'send_sessions', 'auto_notify_exc_info')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
