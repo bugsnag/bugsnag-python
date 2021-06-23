6c3316f5810daf426cea5c327155f4ea899655e5
Joe Haines <hello@joehaines.co.uk>
2021-06-23 13:31:03 +0100

    Bump version to 4.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bugsnag',
-    version='4.0.3',
+    version='4.1.0',
     description='Automatic error monitoring for django, flask, etc.',
     long_description=__doc__,
     author='Simon Maynard',
