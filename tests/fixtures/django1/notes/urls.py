927d4eb2708b3781258fbb09baf51bdabbc795b7
Joe Haines <hello@joehaines.co.uk>
2022-02-15 16:35:38 +0000

    Add tests for chained exceptions in Django

--- tests/fixtures/django1/notes/urls.py
+++ tests/fixtures/django1/notes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'unhandled-crash/', views.unhandled_crash, name='crash'),
+    url(r'unhandled-crash-chain/', views.unhandled_crash_chain),
     url(r'unhandled-template-crash/',
         views.unhandled_crash_in_template),
     url(r'handled-exception/', views.handle_notify),
