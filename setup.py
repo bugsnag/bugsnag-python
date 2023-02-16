7eb1e8d90113855ff11584eea2fcc11b71d0977c
Joe Haines <hello@joehaines.co.uk>
2023-02-16 12:27:07 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bugsnag',
-    version='4.3.0',
+    version='4.4.0',
     description='Automatic error monitoring for django, flask, etc.',
     long_description=__doc__,
     author='Simon Maynard',
