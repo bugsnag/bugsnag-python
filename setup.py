1e09d28388bc34f82e924d4c268ae924d15eda25
Steve Kirkland <steve.kirkland@smartbear.com>
2025-07-08 17:22:58 +0100

    Release v4.8.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bugsnag',
-    version='4.7.1',
+    version='4.8.0',
     description='Automatic error monitoring for django, flask, etc.',
     long_description=__doc__,
     author='Simon Maynard',
