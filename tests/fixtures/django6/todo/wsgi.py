b51db1c35f6fb4a0d182354c9a338402e58f2181
Aleksander Grzegorzewski <aleksander.grzegorzewski@smartbear.com>
2026-01-19 13:07:54 +0100

    added django 5&6 to tests

--- tests/fixtures/django6/todo/wsgi.py
+++ tests/fixtures/django6/todo/wsgi.py
@@ -4,11 +4,13 @@ WSGI config for todo project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+
 application = get_wsgi_application()
